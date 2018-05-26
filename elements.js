05dfe2648446c5413f2e0f6383ddf3e6eb31aa5e
krisxlle <kriselle.t@gmail.com>
2018-05-26 14:14:19 -0700

    width and stats of atoms

--- elements.js
+++ elements.js
@@ -1,6 +1,34 @@
 
 var oxygen = {
-    imgsrc:"o.png"
+    imgsrc:"o.png",
+    anum:8,
+    mass:"16 g/mol",
+    fact:"Because water molecules contain Oxygen, Oxygen makes up most of the mass of all living things!",
+         factsrc:"http://www.softschools.com/facts/periodic_table/oxygen_facts/181/",
+    width:"152%"
+    };
+
+var hydrogen = {
+    imgsrc:"h.png",
+    anum:1,
+    mass:"1 g/mol",
+    fact:"Hydrogen is the most abundant chemical element un the universe.",
+        factsrc:"http://www.softschools.com/facts/periodic_table/hydrogen_facts/177/",
+    width:"120%"
+    };
+
+var carbon = {
+    imgsrc:"c.png",
+    anum:6,
+    mass: "12 g/mol",
+    width: "170%"
+    };
+
+var nitrogen = {
+    imgsrc:"n.png",
+    anum:7,
+    mass:"14 g/mol",
+    width:"155%"
     };
 
 /*
