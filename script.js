760cdec99659a7587983197422a04c1e4fc99d5e
jeanellet <jeanellet@users.noreply.github.com>
2018-05-26 14:21:30 -0700

    fixed styling of first element

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var first=document.getElementById("one");
 
     var newElement=document.createElement("BUTTON");
-    newElement.innerHTML='<img src="'+oxygen.imgsrc+'"/>';
+    newElement.innerHTML='<img src="'+oxygen.imgsrc+'" style="width:'+oxygen.width+'"/>';
 
     first.appendChild(newElement);
 
