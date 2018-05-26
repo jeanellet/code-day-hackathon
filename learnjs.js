b52b88f4790be134ccf6c2f8e0f3c4c792e043e0
krisxlle <kriselle.t@gmail.com>
2018-05-26 15:02:32 -0700

    card divs

---
+++ learnjs.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    var first=document.getElementsByClass("");
+
+    var newElement=document.createElement("BUTTON");
+    newElement.innerHTML='<img src="'+oxygen.imgsrc+'" style="width:'+oxygen.width+'"/>';
+
+    first.appendChild(newElement);
+
+
+
+});
