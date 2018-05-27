46ee803608749fc32493bd6d9e96dff864b53c4a
krisxlle <kriselle.t@gmail.com>
2018-05-26 21:36:03 -0700

    fixed title image sizing

---
+++ title.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+var w = window.innerWidth
+||document.documentElement.clientWidth||document.body.clientWidth;
+var h = window.innerHeight
+||document.documentElement.clientHeight||document.body.clientHeight;
+var scrollWidth =
+    Element.offsetWidth - Element.clientWidth;
+var y = document.getElementById("titleImage");
+        y.style.height = h;
+        y.style.width = w-scrollWidth;
+
+});
