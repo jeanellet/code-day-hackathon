1c84e46f2d433f23b6dd13c718ff0cc396269eeb
jeanellet <jeanellet@users.noreply.github.com>
2018-05-26 17:14:29 -0700

    show selected atoms/ added buttons

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
 
 
 $(document).ready(function(){
+
+    var amountSelected=0;
+
     var first=document.getElementById("one");
 
-    var newElement=document.createElement("BUTTON");
-    newElement.innerHTML='<img src="'+oxygen.imgsrc+'" style="width:'+oxygen.width+'"/>';
+    var newImg=document.createElement("img");
+    newImg.src=oxygen.imgsrc;
+    newImg.className="atomImg";
+    newImg.style="width:"+oxygen.width+"; margin:auto !important;";
+    newImg.align="middle";
+    newImg.id=oxygen.name;
 
-    first.appendChild(newElement);
+    first.appendChild(newImg);
 
+    $(".atomImg").click(function(){
+        var selectedAtom;
+
+        for(i=0;i<array.length;i++){
+            if(array[i].name==this.id){
+                selectedAtom=array[i];
+            }
+            break;
+        }
+
+        console.log("selectedaatom="+selectedAtom.width);
+
+        var currentSlot=amountSelected.toString()+"-select";
+
+        var dest=document.getElementById(currentSlot);
+
+        amountSelected++;
+
+        var copyImg=document.createElement("img");
+        copyImg.src=selectedAtom.imgsrc;
+        copyImg.className="atomImg";
+        copyImg.style="width:"+selectedAtom.width+"; margin:auto !important;";
+        copyImg.align="middle";
+        copyImg.id=selectedAtom.name;
+
+        dest.appendChild(copyImg);
+    });
 
 
 });
